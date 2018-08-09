52d9980e363e482bc33401261788574e10637f31
Adaming <Adaming@192.168.1.134>
2018-08-09 12:09:10 +0200

    Camille : attributs/constructeur/getterSetter class admin, client, commande. association client/commande et Commande/ligneCommande

--- Projet_FunZone/src/fr/adaming/model/Client.java
+++ Projet_FunZone/src/fr/adaming/model/Client.java
@@ -1,21 +1,61 @@
 package fr.adaming.model;
 
+import java.io.Serializable;
+import java.util.List;
 
-public class Client {
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
 
-	//Attributs
+/**
+ * permet de définir un client, Class persistante dans la
+ * base de donnée
+ * Auteur : Camille
+ */
+
+@Entity
+@Table(name = "clients")
+public class Client implements Serializable {
+
+	// Attributs
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id_client")
 	private Long idClient;
 	private String nomClient;
 	private String adresse;
 	private String email;
 	private String tel;
 	private String mdpClient;
+	
+	
+	/**
+	 * Transformation de l'association UML en JAVA
+	 */
+	@OneToMany(mappedBy="client")
+	private List<Commande> listeCommande;
 
-	//constructeur
+	// constructeur
+	/**
+	 * Constructeur vide
+	 */
 	public Client() {
 		super();
 	}
 
+	/**
+	 * Constructeur avec tous les paramètres du client sans id
+	 * 
+	 * @param nomClient
+	 * @param adresse
+	 * @param email
+	 * @param tel
+	 * @param mdpClient
+	 */
 	public Client(String nomClient, String adresse, String email, String tel, String mdpClient) {
 		super();
 		this.nomClient = nomClient;
@@ -25,6 +65,16 @@ public class Client {
 		this.mdpClient = mdpClient;
 	}
 
+	/**
+	 * Constructeur avec tous les paramètres + id
+	 * 
+	 * @param idClient
+	 * @param nomClient
+	 * @param adresse
+	 * @param email
+	 * @param tel
+	 * @param mdpClient
+	 */
 	public Client(Long idClient, String nomClient, String adresse, String email, String tel, String mdpClient) {
 		super();
 		this.idClient = idClient;
@@ -35,53 +85,102 @@ public class Client {
 		this.mdpClient = mdpClient;
 	}
 
-	//getter et setter
+	// getter et setter
+
+	/**
+	 * @return the idClient
+	 */
 	public Long getIdClient() {
 		return idClient;
 	}
 
+	/**
+	 * @param idClient
+	 *            the idClient to set
+	 */
 	public void setIdClient(Long idClient) {
 		this.idClient = idClient;
 	}
 
+	/**
+	 * @return the nomClient
+	 */
 	public String getNomClient() {
 		return nomClient;
 	}
 
+	/**
+	 * @param nomClient
+	 *            the nomClient to set
+	 */
 	public void setNomClient(String nomClient) {
 		this.nomClient = nomClient;
 	}
 
+	/**
+	 * @return the adresse
+	 */
 	public String getAdresse() {
 		return adresse;
 	}
 
+	/**
+	 * @param adresse
+	 *            the adresse to set
+	 */
 	public void setAdresse(String adresse) {
 		this.adresse = adresse;
 	}
 
+	/**
+	 * @return the email
+	 */
 	public String getEmail() {
 		return email;
 	}
 
+	/**
+	 * @param email
+	 *            the email to set
+	 */
 	public void setEmail(String email) {
 		this.email = email;
 	}
 
+	/**
+	 * @return the tel
+	 */
 	public String getTel() {
 		return tel;
 	}
 
+	/**
+	 * @param tel
+	 *            the tel to set
+	 */
 	public void setTel(String tel) {
 		this.tel = tel;
 	}
 
+	/**
+	 * @return the mdpClient
+	 */
 	public String getMdpClient() {
 		return mdpClient;
 	}
 
+	/**
+	 * @param mdpClient
+	 *            the mdpClient to set
+	 */
 	public void setMdpClient(String mdpClient) {
 		this.mdpClient = mdpClient;
 	}
 
+	// ToString
+	@Override
+	public String toString() {
+		return "Client [idClient=" + idClient + ", nomClient=" + nomClient + ", adresse=" + adresse + ", email=" + email
+				+ ", tel=" + tel + ", mdpClient=" + mdpClient + "]";
+	}
 }
--- Projet_FunZone/src/fr/adaming/model/Commande.java
+++ Projet_FunZone/src/fr/adaming/model/Commande.java
@@ -1,5 +1,105 @@
 package fr.adaming.model;
 
-public class Commande {
+import java.io.Serializable;
+import java.util.Date;
+import java.util.List;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+/**
+ * permet de définir une commande, Class persistante dans la base de donnée. Auteur :
+ * Camille
+ */
+@Entity
+@Table(name="commandes")
+public class Commande implements Serializable {
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@Column(name="id_commande")
+	private Long idCommande;
+	
+	@Temporal(TemporalType.TIME)
+	private Date dateCommande;
+
+	
+	/**
+	 * Transformation de l'association UML en JAVA
+	 */
+	@ManyToOne
+	@JoinColumn(name="commande_id", referencedColumnName="id_client")
+	private Client client;
+	
+	/**
+	 * Transformation de l'association UML en JAVA
+	 */
+	@OneToMany(mappedBy="commande")
+	private List<LigneCommande> listeLigneCommande;
+	
+	/**
+	 * Constructeur vide
+	 */
+	public Commande() {
+		super();
+	}
+
+	/**
+	 * Constructeur avec params sans id
+	 * @param dateCommande
+	 */
+	public Commande(Date dateCommande) {
+		super();
+		this.dateCommande = dateCommande;
+	}
+
+	/**
+	 * Constructeur avec params et id
+	 * @param idCommande
+	 * @param dateCommande
+	 */
+	public Commande(Long idCommande, Date dateCommande) {
+		super();
+		this.idCommande = idCommande;
+		this.dateCommande = dateCommande;
+	}
+
+	/**
+	 * @return the idCommande
+	 */
+	public Long getIdCommande() {
+		return idCommande;
+	}
+
+	/**
+	 * @param idCommande the idCommande to set
+	 */
+	public void setIdCommande(Long idCommande) {
+		this.idCommande = idCommande;
+	}
+
+	/**
+	 * @return the dateCommande
+	 */
+	public Date getDateCommande() {
+		return dateCommande;
+	}
+
+	/**
+	 * @param dateCommande the dateCommande to set
+	 */
+	public void setDateCommande(Date dateCommande) {
+		this.dateCommande = dateCommande;
+	}
+
+	
 }
