24c8f4c99e26d242b4df558a0321cf155a52ffe2
Adaming <Adaming@192.168.1.134>
2018-08-10 10:08:51 +0200

    Camille:ManagedBean Commande

--- Projet_FunZone/src/fr/adaming/managedBeans/CommandeManagedBean.java
+++ Projet_FunZone/src/fr/adaming/managedBeans/CommandeManagedBean.java
@@ -5,6 +5,7 @@ import java.io.Serializable;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.RequestScoped;
 
+@SuppressWarnings("serial")
 @ManagedBean
 @RequestScoped
 public class CommandeManagedBean implements Serializable {
